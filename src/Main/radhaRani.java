830891338072b81eb9973a91a57668267ab20bad
Rahul-Chahar <rahul.kumar_cs19@gla.ac.in>
2022-01-26 16:50:52 +0530

    Hare Krishna Hare Krishna Krishna Krishna Hare Hare Hare Rama Hare Rama Rama Rama Hare Hare

---
+++ src/Main/radhaRani.java
@@ -0,0 +1,16 @@
+/*
+ *Author Name: Rahul Chahar
+ *IDE: IntelliJ IDEA Community Edition
+ *Date: 25/01/2022
+ */
+
+package Main;
+
+import Raghunath.bankeyBihari;
+
+public class radhaRani {
+    public static void main(String[] args) {
+        bankeyBihari.bakeyBihari();
+        bankeyBihari.inEnglish();
+    }
+}
---
+++ src/Raghunath/bankeyBihari.java
@@ -0,0 +1,60 @@
+/*
+ *Author Name: Rahul Chahar
+ *IDE: IntelliJ IDEA Community Edition
+ *Date: 25/01/2022
+ */
+
+package Raghunath;
+
+public class bankeyBihari {
+    /**
+     * This method is static without using any parameter
+     * This method giving information about Shri Bankey Bihari ji
+     * All information in hindi.
+     **/
+   public static void bakeyBihari(){
+        System.out.println("Vrindavan ke ho banke bihari hamse parda karo na murari");
+        System.out.println("भगवान श्रीकृष्ण और राधारानी का एकाकार रूप है बांके बिहारी। वृंदावन में बांके बिहारी का प्रसिद्ध मंदिर है। यहां बांके बिहारी की एक झलक पाने के लिए देश विदेश से रोजाना हजारों लोग आते हैं। दर्शन के दौरान अधिकतर भक्त आंखें बंद कर भगवान का ध्यान करते हैं, लेकिन आपको बता दें कि बांके बिहारी जाकर ऐसी भूल मत कीजिएगा, नहीं तो आपका यहां आना व्यर्थ हो जाएगा। आइए आपको बताते हैं हम ऐसा क्यों कह रहे हैं। इसलिए मिलाएं बांके बिहारी से नजरें\n" +
+                "कम लोग ही जानते हैं कि बांके बिहारी की मूर्ति बनाई नहीं गई थी बल्कि ये स्वामी हरिदास के अनुरोध पर प्रकट हुई थी ताकि अन्य लोग भी इसके दर्शन कर भगवान के साक्षात दर्शन कर उनका आशीर्वाद ले सकें। कहा जाता है कि ये मूर्ति किसी धातु की नहीं बल्कि लकड़ी की है। भगवान कृष्ण और माता राधा प्रेम के अधीन हैं जो भी इन्हें प्रेम करता है या इनकी इस मूर्ति की आंखों में आंखें डालकर प्रेम पूर्वक निहारता है, भगवान उस भक्त पर कृपा करने से खुदको नहीं रोक पाते। इसको एक नजर देखने के पीछे एक कथा भी प्रचलित है।\n" +
+                "ये है कथा\n" +
+                "कहा जाता है कि एक बार राजस्थान के एक राजा बांके बिहारी के दर्शन करने वृंदावन आए और वे भगवान बांके बिहारी की नजरों से नजरें मिलाकर उन्हें एक टक देखते रहे। इसके बाद राजा अपने राज्य वापस चले गए। कुछ देर बाद मंदिर के सेवायतों ने मंदिर में आकर देखा तो वहां भगवान मौजूद नहीं थे। इस घटना से मंदिर के पुजारियों में हड़कंप मच गया। भगवान की स्तुति की गई तो पता चला कि भगवान वहां से अपने भक्त के प्रेम के अधीन होकर वहां से उनके साथ ही चले गए हैं। काफी प्रार्थना के बाद भगवान को मंदिर में फिर से स्थापित किया गया। तब से लेकर आज तक यहां किसी को भी भगवान को एकटक देखने की इजाजत नहीं दी जाती है। सेवायत पुजारी इस बात का विशेष खयाल रखते हैं कि कोई भी भक्त इन्हें लगातार न देख सके। इसके लिए मूर्ति के सामने थोड़ी—थोड़ी देर में पर्दा डाला जाता है।\n" +
+                "दर्शन कर स्वयं बहते हैं आंखों से आंसू\n" +
+                "इस मंदिर में बांके बिहारी के दर्शन के लिए आए भक्तों का कहना है कि जब भक्त उनके दर्शन करते हैं तो स्वयं उनके रंग में रंग जाते हैं। उनकी आंखें नम होने लगती हैं, जिसका कारण उन्हें खुद भी पता नहीं होता। उसके बाद यहां बस जाने का या बार बार आने का मन करता है। यहां आए भक्तों को कभी निराशा हाथ नहीं लगती, बांके बिहारी सबकी कामना पूरी करते हैं।");
+    }
+    /**
+     * This method is also static and without using any parameter
+     * This method also giving information about Shri Bankey Bihari ji.
+     * All information in English.
+     */
+    public static void inEnglish(){
+
+        System.out.println("Prince of Vrindavan: Shree Banke Bihari ji Maharaj\n" +
+                "Vrindavan ke ho banke bihari hamse parda karo na murari… goes a bhajan from Vrindavan. Well, Banke Bihari and his parda have a romantic history.\n" +
+                "\n" +
+                "Once upon a time, a princess from Rajasthan came for darshan of Shree Banke Bihari at Vrindavan. She was so enamoured by the prince of Vrindavan that she wanted to remain with Him for the rest of her life. Her family forced her to return to her kingdom. With teary eyes, she last saw Shree Banke Bihari ji and left. Mesmerized by her love for Him, Bihariji followed the princess, and was found missing at His temple home established by Tansen’s guru, Swami Haridas.\n" +
+                "\n" +
+                "Brajwasis mainatin that one can still hear Krishna's flute in Braj...\n" +
+                "A frantic search for Bihariji resulted in Him being found at the princess’s home. This time, Bihariji was coerced back to His temple-home at Vrindavan because He was being hugely missed by all the other devotees who came to meet Him.\n" +
+                "\n" +
+                "From that day, a parda came in between the bhakta and Bhagwan so that due to a particular bhakta’s deep love, Thakurji was not again compelled to follow the devotee home.\n" +
+                "\n" +
+                "Curtains as a security measure!\n" +
+                "Now the parda or curtain keeps the devotees from taking a long look at Him, and melting in devotion. Pujaris regulates the curtain on pretext that it is to ward off evil from the child-Krishna Shree Banke Bihariji.\n" +
+                "\n" +
+                "No bells, please\n" +
+                "It is the only temple where loud temple bells are not used to wake Krishna in the morning. The Brajwasis say that it is not proper to wake a child with a start. He is woken gently. There are no bells even for Aarti, as it might disturb Him.\n" +
+                "\n" +
+                "Night out at Nidhivan\n" +
+                "Once a sevak fell asleep while pulling the strings of a hand fan at night for Shree Banke Bihariji. And woke up with a jerk when he realised that he had missed on his duty, and this might have disturbed Thakurji in his sleep. He peered inside to see if Thakurji was sleeping tight, and was startled to find that He was not there!\n" +
+                "\n" +
+                "It was 1 at night. He kept the vigil, and at 4 in the morning found Thakurji returning laden with sweat and flushed in the face. The sevak did nothing to intrude, and continued with his duty. Next night, he was up and awake all night and found Thakurji leaving at 12 ‘O clock.\n" +
+                "\n" +
+                "Raas utsav - celebrating unison with God\n" +
+                "This time, the sevak sneaked behind Him and found Him entering Nidhivan, and heard the sound of flute and dancing fill the night air. At 4 in the morning Thakurji returned to the temple. Now the sevak knew that Thakurji spent 4 hours each night in raas with gopis. When the Pujari came to rouse Thakurji for mangla arti shortly after He was home, the sevak stopped him from doing so, saying that Thakurji did not get enough sleep because of His nightly excursions to Nidhivan. From then on the time for mangla arti was changed to 8.30 in the morning.\n" +
+                "\n" +
+                "Vraj lore\n" +
+                "Vrindavan is full of tales of Shree Gopijan Vallabh…. Brajwasis mainatin that one can still hear the Lord's flute and hear Him whispering Radha Rani's name, and can also hear the sound of their anklets in Vrindavan.\n" +
+                "\n" +
+                "So, devotees' ode in bhajans like Vrindavan ke ho Banke Bihari hamse parda karo na Murari is certainly heard by naughty Krishna, and a loving relationship continues between God Almighty with His Creation.");
+    }
+}
